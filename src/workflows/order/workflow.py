2ef60f9b05e612ef2262c6f67b6532657890f467
Ubuntu <wasiff@gmail.com>
2025-11-02 21:31:08 -0800

    workflow: shipping

---
+++ src/workflows/order/workflow.py
@@ -0,0 +1,41 @@
+from temporalio import workflow
+from src.activities import order_activities, payment_activities
+from src.workflows.shipping_workflow import ShippingWorkflow
+
+@workflow.defn
+class OrderWorkflow:
+    def __init__(self):
+        self.order = None
+
+    @workflow.run
+    async def run(self, order_id: str, payment_id: str):
+        self.order = await workflow.execute_activity(
+            order_activities.order_received,
+            order_id,
+            schedule_to_close_timeout=3
+        )
+
+        valid = await workflow.execute_activity(
+            order_activities.order_validated,
+            self.order,
+            schedule_to_close_timeout=3
+        )
+
+        await workflow.sleep(3)  # Manual review timer
+
+        payment = await workflow.execute_activity(
+            payment_activities.payment_charged,
+            self.order,
+            payment_id,
+            schedule_to_close_timeout=3
+        )
+
+        if payment["status"] == "charged":
+            await workflow.execute_child_workflow(
+                ShippingWorkflow.run,
+                self.order,
+                id=f"ship-{order_id}",
+                task_queue="shipping-tq",
+            )
+
+        return "Order complete"
