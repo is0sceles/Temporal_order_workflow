70f1e0c264597a739aafa4981c81fdc313b8dd00
Ubuntu <wasiff@gmail.com>
2025-11-03 22:11:32 -0800

    connect to local db

--- src/db/testdb.py
+++ src/db/testdb.py
@@ -1,15 +1,27 @@
-import aiomysql
-import asyncio
 import os
+import asyncio
+import aiomysql
+from dotenv import load_dotenv
+
+load_dotenv()
 
 async def check_db():
+    host = os.getenv("DB_HOST")
+    port = int(os.getenv("DB_PORT"))
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    db_name = os.getenv("DB_NAME", "order_app")
+
+    print("Connecting to:", host, port, db_name)
+
     conn = await aiomysql.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT", 3360)),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        db=os.getenv("DB_NAME", "order_app")
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        db=db_name
     )
+
     async with conn.cursor() as cur:
         await cur.execute("SELECT DATABASE();")
         db = await cur.fetchone()
